b98a77bf5112e53acf8e6b92a35b137c7d70f173
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2022-12-27 21:59:46 +0900

    [Bronze V] Title: A+B - 8, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,12 @@
+count = int(input())
+
+i = 0;
+AA =[]
+BB = []
+for i in range(count):
+    A, B = map(int, input().split())
+    AA.append(A)
+    BB.append(B)
+
+for i in range(count):
+    print('Case #%d: %d + %d = %d' %(i+1,AA[i],BB[i],AA[i]+BB[i]))
