1269f9c374dd8a3be8c21c8775d23467b1d3b617
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2023-10-23 21:12:18 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = []
+B = []
+A = list(map(int,input().split()))
+B = list(map(int,input().split())) 
+
+A.sort()
+sum = 0
+for i in range(N):
+    sum += A[i] * max(B)
+    B.remove(max(B))
+print(sum)
+    
