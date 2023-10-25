a82ba2950559444ebd37b7e23fb30cecb3d4e3f0
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2023-10-25 19:51:42 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,19 +1,9 @@
 T = int(input())
-
+a = [25,10,5,1]
 
 
 for i in range(T):
-    q = 0
-    d = 0
-    n = 0
-    p = 0
     C = int(input())
-    q += int(C // 25)
-    C %= 25
-    d += int(C // 10)
-    C %= 10
-    n += int(C // 5)
-    C %= 5
-    p += int(C // 1)
-    C %= 1
-    print(q, d, n, p)
+    for i in range(len(a)):
+        print(C//a[i], end = ' ')
+        C %= a[i]
