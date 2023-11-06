32b413567cb3aba50f9772ae300c90d3414b5adf
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2023-11-06 19:39:31 +0900

    [Bronze III] Title: 우유 축제, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.py
+++ 백준/Bronze/14720. 우유 축제/우유 축제.py
@@ -1,15 +1,9 @@
 N = int(input())
 
 a = list(map(int,input().split()))
-b = a.index(0)
-    
-count =1
-temp = a[b]
-for i in range(b,N):
-    if temp+1 == a[i]:
+count = 0
+
+for i in range(N):
+    if a[i] == count%3:
         count += 1
-        temp = a[i]
-    elif temp == a[i] + 2 :
-        count +=1
-        temp = a[i]
 print(count)
