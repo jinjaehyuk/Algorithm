708a5c70c9f7d0f4f1824df376fa429b92a0fe8f
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2023-11-16 22:27:47 +0900

    Update 컵홀더.py

--- 백준/Bronze/2810. 컵홀더/컵홀더.py
+++ 백준/Bronze/2810. 컵홀더/컵홀더.py
@@ -1,20 +1,24 @@
 N = int(input())    
 seat = list(input())
-b = []
-b.append('*')
-for i in range(len(seat)):
-    if seat[i] == 'S':
-        b.append(seat[i])
-        b.append('*')
-    elif seat[i] =='L':
-        if b[len(b)-1] !='L':
-            b.append(seat[i])
-        else:
-            b.append(seat[i])
-            b.append('*')
+#b = []
+#b.append('*')
+#for i in range(len(seat)):
+#    if seat[i] == 'S':
+#        b.append(seat[i])
+#        b.append('*')
+#    elif seat[i] =='L':
+#        if b[len(b)-1] !='L':
+#            b.append(seat[i])
+#        else:
+#            b.append(seat[i])
+#            b.append('*')
 
-a = b.count('*')
-if N > a:
-    print(a)
-else:
-    print(N)
+#a = b.count('*')
+#if N > a:
+#    print(a)
+#else:
+#    print(N)
+llcount = seat.count("LL") -1
+if llcount < 0 : llcount = 0
+
+print(N-llcount)
