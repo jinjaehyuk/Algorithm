9bdec020026eb92e80b538381f1002e3ad464e86
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2023-12-15 15:16:17 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 31452 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,12 +1,53 @@
-import sys
-from collections import deque
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+
+# dx = [-1,1,0,0,-1,-1,1,1]
+# dy = [0,0,-1,1,-1,1,-1,1]
+# result = []
+# while True:
+    
+#     w,h = map(int,input().split())
+    
+#     if w == 0 and h == 0:
+#         break
+    
+#     graph=[]
+#     for i in range(h):
+#         graph.append(list(map(int,input().split())))
+    
+#     def bfs(x,y):
+#         queue = deque()
+#         queue.append((x,y))
+#         graph[x][y] = 0
+#         count = 1
+#         while queue:
+#             x, y = queue.popleft()
+#             for i in range(8):
+#                 nx = x + dx[i]
+#                 ny = y + dy[i]
+#                 if 0 <= nx < h and 0<= ny < w and graph[nx][ny] == 1:
+#                     graph[nx][ny] = 0
+#                     queue.append((nx, ny))
+#                     count += 1
+#         return count  
+#     cnt = []
+#     for i in range(h):
+#         for j in range(w):
+#             if graph[i][j] == 1:
+#                 cnt.append(bfs(i,j))
+#     result.append(len(cnt))
+# for i in result:
+#     print(i)
+import sys 
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
 dx = [-1,1,0,0,-1,-1,1,1]
 dy = [0,0,-1,1,-1,1,-1,1]
 result = []
 while True:
-    
     w,h = map(int,input().split())
     
     if w == 0 and h == 0:
@@ -15,27 +56,24 @@ while True:
     graph=[]
     for i in range(h):
         graph.append(list(map(int,input().split())))
-    
-    def bfs(x,y):
-        queue = deque()
-        queue.append((x,y))
+    count = 0
+    def dfs(x,y):
+        global count
         graph[x][y] = 0
-        count = 1
-        while queue:
-            x, y = queue.popleft()
-            for i in range(8):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < h and 0<= ny < w and graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
-                    queue.append((nx, ny))
-                    count += 1
-        return count  
+        count += 1
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < h and 0<= ny < w and graph[nx][ny] == 1:
+                dfs(nx,ny)
+        return count      
+    
     cnt = []
     for i in range(h):
         for j in range(w):
             if graph[i][j] == 1:
-                cnt.append(bfs(i,j))
+                cnt.append(dfs(i,j))
+                count = 0
     result.append(len(cnt))
 for i in result:
-    print(i)             
+    print(i) 
