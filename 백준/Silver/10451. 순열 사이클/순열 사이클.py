72434b7d53dab4f6112d5b81acfac35ad5e208e9
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2024-01-03 10:11:29 +0900

    [Silver III] Title: 순열 사이클, Time: 648 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -11,7 +11,39 @@ def dfs(v):
     for i in graph[v]:
         if not visited[i]:
             dfs(i)
+       
+def bfs(v):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True     
+# for _ in range(T):
+#     a = []
+#     a_temp = []
+#     n = int(input())
+#     a = list(map(int,input().split()))    
+#     a_temp = sorted(a)
+#     graph = [[] for  _ in range(n+1)]
+#     for i in range(n):
+#         x, y = a_temp[i], a[i]
+#         graph[x].append(y)
+#         graph[y].append(x)
             
+#     visited = [False]* (n+1)
+    
+#     count = -1
+    
+#     for i in range(n+1):
+#         if not visited[i]:
+#             dfs(i)
+#             count += 1
+#     print(count)
+    
+
 for _ in range(T):
     a = []
     a_temp = []
@@ -30,8 +62,6 @@ for _ in range(T):
     
     for i in range(n+1):
         if not visited[i]:
-            dfs(i)
+            bfs(i)
             count += 1
     print(count)
-    
-    
