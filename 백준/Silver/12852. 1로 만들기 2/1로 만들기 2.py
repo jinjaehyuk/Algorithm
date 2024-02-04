cbd90676cfd9a1763b9bf6b2a73fb315a9615a9d
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2024-02-04 14:21:54 +0900

    [Silver I] Title: 1로 만들기 2, Time: 540 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n= int(input())
+d = [0] * (n+1)
+for i in range(2,n+1):
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i],d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i],d[i//3]+1)
+
+print(d[n])
+
+path = []
+while n != 0:
+    print(n,end =" ")
+    if n%3 == 0 and d[n] == d[n//3]+1:
+        n //= 3
+    elif n%2 ==0 and d[n] == d[n//2]+1:
+        n//=2
+    else:
+        n -= 1
