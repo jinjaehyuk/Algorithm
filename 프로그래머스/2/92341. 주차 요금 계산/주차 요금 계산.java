5c21321499063569c14478de644c625437b7d0b3
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2024-03-15 15:26:53 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.13 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -7,9 +7,9 @@ class Solution {
     static final String LAST_TIME= "23:59";
 
     public int[] solution(int[] fees, String[] records) {
-        String[][] memo = new String[records.length][3]; //입/출차 기록 
-        HashMap<Integer,String> manage = new HashMap<Integer, String>(); // 입 출차 관리 맵
-        TreeMap<Integer, Integer> useTime = new TreeMap<Integer, Integer>();//최종 사용 시간 맵
+                String[][] memo = new String[records.length][3]; //입/출차 기록 
+        HashMap<String,String> manage = new HashMap<String, String>(); // 입 출차 관리 맵
+        TreeMap<String, Integer> useTime = new TreeMap<String, Integer>();//최종 사용 시간 맵
         //입출차 기록
         for(int i =0; i< records.length; i ++){
             memo[i] = records[i].split(" ");
@@ -18,29 +18,29 @@ class Solution {
         //입 출차 관리
         for(int i =0; i < memo.length; i ++){
             if(memo[i][2].equals("IN")){
-                manage.put(Integer.parseInt(memo[i][1]),memo[i][0]);
+                manage.put(memo[i][1],memo[i][0]);
             }
             else if(memo[i][2].equals("OUT")){
-                int minutes = timeCal(manage.get(Integer.parseInt(memo[i][1])),memo[i][0]);
+                int minutes = timeCal(manage.get(memo[i][1]),memo[i][0]);
 
                 //최종 시간 계산
-                if( useTime.containsKey(Integer.parseInt(memo[i][1])) ){
-                    useTime.put(Integer.parseInt(memo[i][1]), useTime.get(Integer.parseInt(memo[i][1])) + minutes);
+                if( useTime.containsKey(memo[i][1]) ){
+                    useTime.put(memo[i][1], useTime.get(memo[i][1]) + minutes);
                 }
                 else{
-                    useTime.put(Integer.parseInt(memo[i][1]), minutes);
+                    useTime.put(memo[i][1], minutes);
 
                 }
 
                //출차된 차 기록삭제
-                manage.remove(Integer.parseInt(memo[i][1]));
+                manage.remove(memo[i][1]);
             }
         }
         if(!manage.isEmpty()){
-            Set<Integer> keySet = manage.keySet();
-            for(Integer key : keySet){
+            Set<String> keySet = manage.keySet();
+            for(String key : keySet){
                 int addTime = timeCal(manage.get(key),LAST_TIME);
-                if(!useTime.containsKey(key)){
+                if( !useTime.containsKey(key)){
                     useTime.put(key, addTime);
                 }else{
                     useTime.put(key, useTime.get(key) + addTime);
@@ -48,9 +48,9 @@ class Solution {
             }
         }
         int[] answer = new int[useTime.size()];
-        Set<Integer> carNums = useTime.keySet();
+        Set<String> carNums = useTime.keySet();
         int answerIdx =0;
-        for(Integer carNum : carNums){
+        for(String carNum : carNums){
             int totalMin = useTime.get(carNum);
             if(fees[0] >= totalMin){
                 answer[answerIdx] = fees[1];
