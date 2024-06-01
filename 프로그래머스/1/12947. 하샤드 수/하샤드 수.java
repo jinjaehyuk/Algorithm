51f77629fbbedb73bcf3c8bc1423868787fa3906
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2024-06-01 15:38:00 +0900

    [level 1] Title: 하샤드 수, Time: 0.12 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String[] arr = (String.valueOf(x)).split("");
+        int sum = 0;
+        for(int i =0; i < arr.length; i ++){
+            sum += Integer.parseInt(arr[i]);
+        }
+        if(x%sum != 0 ) answer= false;
+        return answer;
+    }
+}
