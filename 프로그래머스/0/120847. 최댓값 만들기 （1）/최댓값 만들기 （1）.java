e157280788264934c0e6f71b4ecdec20b2a8d4ff
Jin.JH <68697549+jinjaehyuk@users.noreply.github.com>
2024-12-28 17:29:34 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.13 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int max = 0;
+        for(int i =0 ; i < numbers.length; i ++){
+            for(int j = i+1; j< numbers.length; j ++){
+                max = numbers[i] * numbers[j];
+                if(max > answer){
+                   answer = max;
+                }
+            }
+        }
+        return answer;
+    }
+}
